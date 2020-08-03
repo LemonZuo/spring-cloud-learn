821fe47e9cd10a6a9d88de4ad13f760224d8c31a
LemonZuo <lemonzuo@qq.com>
2020-08-03 10:52:32 +0800

    初始化支付服务提供者

---
+++ cloud-provider-payment8001/src/main/java/com/lemonzuo/PaymentApplication.java
@@ -0,0 +1,15 @@
+package com.lemonzuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LemonZuo
+ * @create 2020-08-02 12:24
+ */
+@SpringBootApplication
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
