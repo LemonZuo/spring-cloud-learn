f17d9b3458fb09055a19816854ccb05d032b67f0
LemonZuo <lemonzuo@qq.com>
2021-02-10 18:00:34 +0800

    Hystrix图表监控

---
+++ cloud-consumer-hystrix-9001/src/main/java/com/lemonzuo/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.lemonzuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author LemonZuo
+ * @create 2021-02-10 17:42
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
---
+++ cloud-provider-payment-hystrix-8001/src/main/java/com/lemonzuo/config/HystrixDashboardConfig.java
@@ -0,0 +1,23 @@
+package com.lemonzuo.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author LemonZuo
+ * @create 2021-02-10 17:49
+ */
+@Configuration
+public class HystrixDashboardConfig {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
