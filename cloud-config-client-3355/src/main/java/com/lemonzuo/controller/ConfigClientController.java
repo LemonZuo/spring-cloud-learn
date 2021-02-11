cbba3455ba8a6932a42f67eb04f3f2536da267b7
LemonZuo <lemonzuo@qq.com>
2021-02-11 11:54:19 +0800

    Config配置中心客户端

---
+++ cloud-config-client-3355/src/main/java/com/lemonzuo/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package com.lemonzuo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author LemonZuo
+ * @create 2021-02-11 11:27
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
