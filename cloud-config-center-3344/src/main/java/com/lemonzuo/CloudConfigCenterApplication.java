b5a52616086e6d837436d31464614a32b277a1d1
LemonZuo <lemonzuo@qq.com>
2021-02-11 11:16:02 +0800

    Config配置中心

---
+++ cloud-config-center-3344/src/main/java/com/lemonzuo/CloudConfigCenterApplication.java
@@ -0,0 +1,19 @@
+package com.lemonzuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author LemonZuo
+ * @create 2021-02-11 10:38
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class CloudConfigCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigCenterApplication.class, args);
+    }
+}
