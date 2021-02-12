c7e9d857037ab8173727abb85efb1d700eed0506
LemonZuo <lemonzuo@qq.com>
2021-02-12 22:48:52 +0800

    修改Nacos服务提供者业务类

--- cloud-alibaba-nacos-provider-payment-9001/src/main/java/com/lemonzuo/controller/PaymentController.java
+++ cloud-alibaba-nacos-provider-payment-9001/src/main/java/com/lemonzuo/controller/PaymentController.java
@@ -1,5 +1,8 @@
 package com.lemonzuo.controller;
 
+import com.lemonzuo.model.CommonResult;
+import com.lemonzuo.model.Payment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PaymentController {
-    @GetMapping(value = "/echo/{string}")
-    public String echo(@PathVariable String string) {
-        return "Hello Nacos Discovery " + string;
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/payment/get/{id}")
+    public String getPaymentById(@PathVariable Long id) {
+        StringBuffer result = new StringBuffer("端口号：")
+                .append(port)
+                .append(" ,id:")
+                .append(id);
+        return result.toString();
     }
 }
--- cloud-alibaba-nacos-provider-payment-9002/src/main/java/com/lemonzuo/controller/PaymentController.java
+++ cloud-alibaba-nacos-provider-payment-9002/src/main/java/com/lemonzuo/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.lemonzuo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PaymentController {
-    @GetMapping(value = "/echo/{string}")
-    public String echo(@PathVariable String string) {
-        return "Hello Nacos Discovery " + string;
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/payment/get/{id}")
+    public String getPaymentById(@PathVariable Long id) {
+        StringBuffer result = new StringBuffer("端口号：")
+                .append(port)
+                .append(" ,id:")
+                .append(id);
+        return result.toString();
     }
 }
