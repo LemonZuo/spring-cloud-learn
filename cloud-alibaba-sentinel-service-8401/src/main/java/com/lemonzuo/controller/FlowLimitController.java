f49efd37734b502569d3929d7859664bccbcb70d
LemonZuo <lemonzuo@qq.com>
2021-02-16 13:21:32 +0800

    Sentinel热点规则

--- cloud-alibaba-sentinel-service-8401/src/main/java/com/lemonzuo/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service-8401/src/main/java/com/lemonzuo/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.lemonzuo.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,5 +20,23 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
         return "testB";
+    }
+
+    @GetMapping("/hotKey")
+    @SentinelResource(value = "hotKey", blockHandler = "dealHotKey")
+    public String hotKey(@RequestParam(value = "p1", required = false) String p1,
+                         @RequestParam(value = "p1", required = false) String p2) {
+        return "====hot key====";
+    }
+
+    /**
+     * 降级处理方法
+     * @param p1
+     * @param p2
+     * @param exception
+     * @return
+     */
+    public String dealHotKey(String p1, String p2, BlockException exception) {
+        return "====dealHotKey====";
     }
 }
